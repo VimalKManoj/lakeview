da0efc67dd08efd4352592742bae6c2755fd9cee
VimalKManoj <vimalkmanoj@gmail.com>
2024-09-01 21:25:29 +0530

    second commit

--- app/Gallery/loading.js
+++ app/Gallery/loading.js
@@ -6,7 +6,7 @@ const loading = () => {
  	<span class='sr-only'>Loading...</span>
   	<div class='h-4 w-4 bg-[#FDD604] rounded-full animate-bounce [animation-delay:-0.3s]'></div>
 	<div class='h-4 w-4 bg-[#FDD604] rounded-full animate-bounce [animation-delay:-0.15s]'></div>
-	<div class='h-4 w-4 bg-[#FDD604] rounded-full animate-bounce'></div>
+	<div class='h-4 w-4 bg-[#FDD604]  rounded-full animate-bounce'></div>
 </div>
   )
 }
